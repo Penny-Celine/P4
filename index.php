799c33a970e24685421398014dbc0711d0876fe0
Céline Maupoux <celine.maupoux@gmail.com>
2020-06-21 15:06:58 +0200

    fix index.php

--- index.php
+++ index.php
@@ -32,7 +32,6 @@ if (isset($_GET['page'])) {
             break;
         case 'deconnexion' :
             session_destroy();
-            $_GET['page']='accueil';
             $page->displayHomePage();
             break;
         default:
